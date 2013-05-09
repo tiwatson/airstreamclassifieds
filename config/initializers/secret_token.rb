4cfa78414eb28d27f786e6d29c579ccca3432764
Tim Watson <tiwatson@gmail.com>
2013-05-09 14:18:30 -0600

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Airstreamclassifieds::Application.config.secret_token = '43ab65e2fd0b6a439df163a752bb4dca4963a6166d66996f707fe6fe9e3f7f9da9646e8f6da05d1dc6659211fb3660fd53d2f214f1612cb5ead4d8ddc671416a'
+Airstreamclassifieds::Application.config.secret_token = '0d7ae5c4eb3c34e8c0a1d8258bbc6c5c8d8fda72b675a0aa291ef9772b493db7b9a97a68fa766c2cf58417bb4b9c7d4640f992bbec81e24e13e3c90c1c2cf759'
