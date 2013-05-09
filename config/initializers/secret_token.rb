a36508fc49660cbc4146b04ea2e450155abf3930
Tim Watson <tiwatson@gmail.com>
2013-05-09 13:17:18 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airstreamclassifieds::Application.config.secret_token = '4f812a4f476328991a7e24a291ec5359424a1136438febe841dd31fea32e842d960f84567daac31e3ab2a4d8b953c95e014527f94d6ec7d110f41110925ed963'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Airstreamclassifieds::Application.config.session_store :cookie_store, key: '_airstreamclassifieds_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Airstreamclassifieds::Application.config.session_store :active_record_store
